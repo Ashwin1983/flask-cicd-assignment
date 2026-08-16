8a51fb8fe0a331222356bd303365c80e1f7e4cc4
Ashwin <ramachandra.ashwin@gmail.com>
2026-08-16 07:42:38 +0530

    Intentionally break health test

--- tests/test_app.py
+++ tests/test_app.py
@@ -24,7 +24,7 @@ def test_home_success(client):
 def test_health_success(client):
     response = client.get("/health")
 
-    assert response.status_code == 200
+    assert response.status_code == 500
 
     data = response.get_json()
 
